3869584c1f9bfde191ce2f363083ab5888f364dc
estanisalpre <estanisprevite@hotmail.com>
2026-08-05 15:12:34 -0500

    feat: reestructure folders and files

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-const SPEED := 100.0
+const SPEED := 70.0
 
 func _physics_process(_delta: float) -> void:
 	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * SPEED
 	move_and_slide()
+
+func _ready():
+	global_position = get_tree().current_scene.get_node("World/SpawnPlayer").global_position
